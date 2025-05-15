ed10c281e21e793c5c460aaecf5fb95c3817a836
Vinicius <viniciusmarques.dev@gmail.com>
2025-05-15 10:21:19 -0300

    feat: first commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math/big"
+	"net/http"
+	"sync"
+)
+
+var (
+	urlStore    = make(map[string]string)
+	mu          sync.Mutex
+	secretKey   = []byte("secretaeskey123456789123456789")
+	lettersRune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+func encrypt(originalUrl string) string {
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plainText := []byte(originalUrl)
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+
+	iv := cipherText[:aes.BlockSize]
+
+	if _, err := rand.Read(iv); err != nil {
+		log.Fatal(err)
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+	return hex.EncodeToString(cipherText)
+}
+
+func generateShortId() string {
+	b := make([]rune, 6)
+	for i := range b {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(lettersRune))))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b[i] = lettersRune[num.Int64()]
+	}
+
+	return string(b)
+}
+
+func shortenUrl(w http.ResponseWriter, r *http.Request) {
+	originalUrl := r.URL.Query().Get("url")
+	if originalUrl == "" {
+		http.Error(w, "URL obrigatório", http.StatusBadRequest)
+		return
+	}
+
+	encryptedUrl := encrypt(originalUrl)
+	shortId := generateShortId()
+	mu.Lock()
+	urlStore[shortId] = encryptedUrl
+	mu.Unlock()
+
+	shortUrl := fmt.Sprintf("http://localhost:4050/%s", shortId)
+	fmt.Fprintf(w, "A URL encurtada desta url original é: %s", shortUrl)
+}
+
+func main() {
+	http.HandleFunc("/shorten", shortenUrl)
+
+	fmt.Println("Projeto iniciando e rodando na porta 4050")
+	log.Fatal(http.ListenAndServe(":4050", nil))
+}
